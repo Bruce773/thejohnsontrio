4a0c839c571d691f0f5b5a74de827cddba1b5f9f
Bruce <bruce@yogainternational.com>
2019-10-02 22:52:40 -0400

    adds even more project setup files

---
+++ src/App.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { MainHeader } from './elements';
+
+export const App = () => <MainHeader>The Johnson Trio</MainHeader>;
---
+++ src/elements.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { mainGreen } from './ColorVars';
+
+export const MainHeader = styled.div`
+  font-size: 25px;
+  text-align: center;
+  color: ${mainGreen};
+`;
